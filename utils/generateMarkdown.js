804d943cf6be450de3d53fe2ab4c6d1ec65af337
Krushil Naik <krushilnaik96@gmail.com>
2021-10-10 10:23:40 -0400

    Resolve #7

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,7 +20,19 @@ function renderLicenseBadge(license) {
 
 // TODO: #7 Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+	const links = {
+		'Apache License 2.0': 'https://opensource.org/licenses/Apache-2.0',
+		'Boost Software License 1.0': 'https://www.boost.org/LICENSE_1_0.txt',
+		'GNU General Public License': 'http://perso.crans.org/besson/LICENSE.html',
+		'GNU General Public License 2.0': 'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html',
+		'MIT License': 'https://opensource.org/licenses/MIT',
+		'Mozilla Public License 2.0': 'https://opensource.org/licenses/MPL-2.0',
+		'Unlicense License': 'http://unlicense.org/'
+	};
+
+	return links[license];
+}
 
 // TODO: #8 Create a function that returns the license section of README
 // If there is no license, return an empty string
