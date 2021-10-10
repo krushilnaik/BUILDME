7fd9b1d607021d599fc7cd83ab135de28f728d17
Krushil Naik <krushilnaik96@gmail.com>
2021-10-10 10:23:50 -0400

    Resolve #8

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -36,7 +36,9 @@ function renderLicenseLink(license) {
 
 // TODO: #8 Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+	return license ? `[![${license}](${renderLicenseBadge(license)})](${renderLicenseLink(license)})` : '';
+}
 
 // TODO: #9 Create a function to generate markdown for README
 function generateMarkdown(data) {
