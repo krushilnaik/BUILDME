04619c3130f6cb35eaeba9487bff8921096537bb
Krushil Naik <krushilnaik96@gmail.com>
2021-10-10 10:23:28 -0400

    Resolve #6

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,22 @@
 // TODO: #6 Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+/**
+ * @param {string} license
+ */
+function renderLicenseBadge(license) {
+	const badges = {
+		'Apache License 2.0': 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
+		'Boost Software License 1.0': 'https://img.shields.io/badge/License-Boost%201.0-lightblue.svg',
+		'GNU General Public License': 'https://img.shields.io/badge/License-GPL-blue.svg',
+		'GNU General Public License 2.0': 'https://img.shields.io/badge/License-GPL%20v2-blue.svg',
+		'MIT License': 'https://img.shields.io/badge/License-MIT-yellow.svg',
+		'Mozilla Public License 2.0': 'https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg',
+		'Unlicense License': 'https://img.shields.io/badge/license-Unlicense-blue.svg'
+	};
+
+	return badges[license];
+}
 
 // TODO: #7 Create a function that returns the license link
 // If there is no license, return an empty string
